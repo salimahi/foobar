8e1a16c081509bb0a4279599a9827355e6773c79
salimahi <76531234+salimahi@users.noreply.github.com>
2023-11-13 15:11:48 -0500

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,4 +1,12 @@
 def solution(x):
+    x = list(x)
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    switch = str.maketrans(alphabet, alphabet[::-1])
-    return x.translate(switch)
+    opposite = alphabet[::-1]
+    cipher = {}
+    for i in range(len(alphabet)):
+        cipher[alphabet[i]] = opposite[i] 
+    for j in range(len(x)):
+        if x[j] in cipher.keys():
+            x[j] = cipher[x[j]]
+        
+    print(''.join(x))
